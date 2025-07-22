5dcd4fd47cdfb9e91f6cdcbb211606097e79c8c3
digitalisius <victoria.arunika@gmail.com>
2025-07-22 17:27:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// File: config.js
+// FILE INI BERISI DATA RAHASIA. JANGAN DIUNGGAH KE GITHUB.
+
+// 1. Kredensial Login Aplikasi
+const APP_USER = "admin1";
+const APP_PASS = "Pnkudus123";
+
+// 2. Kunci Konfigurasi Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyC4IMkGdgUUlEAIDeTrGE1dDF6HeloaxNQ",
+    authDomain: "simanis-423b3.firebaseapp.com",
+    projectId: "simanis-423b3",
+    storageBucket: "simanis-423b3.appspot.com",
+    messagingSenderId: "1094491007403",
+    appId: "1:1094491007403:web:03cb887160f8aa7ab60aee"
+};
